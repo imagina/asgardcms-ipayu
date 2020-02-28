89190e3ace1dbb33436e4f5795a868548840e5e9
YeisonTapia <yftapial@ut.edu.co>
2020-02-28 18:40:03 -0500

    Add AdditionalChargesApiController methods implementations.

--- Http/Controllers/Api/AdditionalChargesApiController.php
+++ Http/Controllers/Api/AdditionalChargesApiController.php
@@ -19,4 +19,60 @@ class AdditionalChargesApiController
     $this->payUAdditionalCharges = $payUAdditionalCharges;
   }
 
+  public function create (Request $request)
+  {
+    try {
+      $data = $request->input('attributes');
+      $plan = $this->payUAdditionalCharges->creation($data);
+      $response = ["data" => $plan];
+    } catch (\Exception $e) {
+
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    return response()->json($response, $status ?? 200);
+  }
+
+  public function update ($criteria, Request $request)
+  {
+    try {
+      $data = $request->input('attributes');
+      $plan = $this->payUAdditionalCharges->update($criteria, $data);
+      $response = ["data" => $plan];
+    } catch (\Exception $e) {
+
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    return response()->json($response, $status ?? 200);
+  }
+
+  public function show ($criteria)
+  {
+    try{
+      $data = $this->payUAdditionalCharges->query($criteria);
+      $response = [
+        'data' => $data
+      ];
+      $status = 200;
+    }catch (PayUException $e){
+      $status = 500;
+      $response = ["errors" => $e];
+    }
+    return response()->json($response, $status);
+  }
+
+  public function delete ($criteria)
+  {
+    try {
+      $plan = $this->payUAdditionalCharges->delete($criteria);
+      $response = ["data" => $plan];
+    } catch (\Exception $e) {
+
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    return response()->json($response, $status ?? 200);
+  }
+
 }
