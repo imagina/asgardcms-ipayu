3ce8e1c5d03979af88cec2daa6848eae9cfce92d
YeisonTapia <yftapial@ut.edu.co>
2020-02-28 18:15:40 -0500

    Add setting for data configuration payu

---
+++ Config/settings.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+  'payUisTest' => [
+    'description' => 'ipayu::common.settings.payUisTest',
+    'view' => 'checkbox',
+  ],
+  'payUapiKey' => [
+      'description' => 'ipayu::common.settings.payUapiKey',
+      'view' => 'text'
+  ],
+  'payUapiLogin' => [
+      'description' => 'ipayu::common.settings.payUapiLogin',
+      'view' => 'text'
+  ],
+  'payUmerchantId' => [
+      'description' => 'ipayu::common.settings.payUmerchantId',
+      'view' => 'text'
+  ],
+  'payUlanguage' => [
+    'description' => 'ipayu::common.settings.payUlanguage',
+    'view' => 'text'
+  ],
+  'setPaymentsCustomUrl' => [
+    'description' => 'ipayu::common.settings.setPaymentsCustomUrl',
+    'view' => 'text'
+  ],
+  'setReportsCustomUrl' => [
+    'description' => 'ipayu::common.settings.setReportsCustomUrl',
+    'view' => 'text'
+  ],
+  'setSubscriptionsCustomUrl' => [
+    'description' => 'ipayu::common.settings.setSubscriptionsCustomUrl',
+    'view' => 'text'
+  ]
+];
