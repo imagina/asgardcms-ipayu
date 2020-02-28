2fe68745c6ce945957e9d1de0b3fe5bd2fb84a4c
YeisonTapia <yftapial@ut.edu.co>
2020-02-28 18:20:28 -0500

    Add translations

---
+++ Resources/lang/en/common.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+  'settings' => [
+    'payUapiKey' => 'Api Key PayU',
+    'payUapiLogin' => 'Api login PayU',
+    'payUmerchantId' => 'MerchantId PayU',
+    'payUlanguage' => 'LanguagePayU',
+    'payUlanguageEs' => 'Spanish',
+    'payUlanguageEn' => 'English',
+    'payUisTest' => 'Test mode',
+    'payUisTestTrue' => 'Yes',
+    'payUisTestFalse' => 'No',
+    'setPaymentsCustomUrl' => 'Payments URL',
+    'setReportsCustomUrl' => 'Queries URL',
+    'setSubscriptionsCustomUrl' => 'Subscriptions for recurring payments URL',
+  ]
+];
---
+++ Resources/lang/es/common.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+  'settings' => [
+      'payUapiKey' => 'Api Key PayU',
+      'payUapiLogin' => 'Api login PayU',
+      'payUmerchantId' => 'Identificador de la tienda PayU',
+      'payUlanguage' => 'Idioma PayU',
+      'payUlanguageEs' => 'Español',
+      'payUlanguageEn' => 'Ingles',
+      'payUisTest' => 'Modo test activado',
+      'payUisTestTrue' => 'Si',
+      'payUisTestFalse' => 'No',
+      'setPaymentsCustomUrl' => 'URL de Pagos',
+      'setReportsCustomUrl' => 'URL de Consultas',
+      'setSubscriptionsCustomUrl' => 'URL de Suscripciones para Pagos Recurrentes',
+  ]
+];
