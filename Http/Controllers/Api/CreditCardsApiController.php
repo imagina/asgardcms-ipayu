2f6209ae4427faf6c6aaedcf452bfda778226f58
YeisonTapia <yftapial@ut.edu.co>
2020-02-28 18:39:02 -0500

    Add CreditCardsApiController methods implementations.

--- Http/Controllers/Api/CreditCardsApiController.php
+++ Http/Controllers/Api/CreditCardsApiController.php
@@ -19,4 +19,60 @@ class CreditCardsApiController
     $this->payUCreditCards = $payUCreditCards;
   }
 
+  public function create (Request $request)
+  {
+    try {
+      $data = $request->input('attributes');
+      $plan = $this->payUCreditCards->creation($data);
+      $response = ["data" => $plan];
+    } catch (\Exception $e) {
+
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    return response()->json($response, $status ?? 200);
+  }
+
+  public function update ($criteria, Request $request)
+  {
+    try {
+      $data = $request->input('attributes');
+      $plan = $this->payUCreditCards->update($criteria, $data);
+      $response = ["data" => $plan];
+    } catch (\Exception $e) {
+
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    return response()->json($response, $status ?? 200);
+  }
+
+  public function show ($criteria)
+  {
+    try{
+      $data = $this->payUCreditCards->query($criteria);
+      $response = [
+        'data' => $data
+      ];
+      $status = 200;
+    }catch (PayUException $e){
+      $status = 500;
+      $response = ["errors" => $e];
+    }
+    return response()->json($response, $status);
+  }
+
+  public function delete ($criteria)
+  {
+    try {
+      $plan = $this->payUCreditCards->delete($criteria);
+      $response = ["data" => $plan];
+    } catch (\Exception $e) {
+
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    return response()->json($response, $status ?? 200);
+  }
+
 }
