15c55159ddfee11e8a24c37fffc79815cc04e561
YeisonTapia <yftapial@ut.edu.co>
2020-03-03 15:56:34 -0500

    Update subscription routes

--- Http/ApiRoutes/subscriptionRoutes.php
+++ Http/ApiRoutes/subscriptionRoutes.php
@@ -9,16 +9,6 @@ $router->group(['prefix' => 'subscriptions'], function (Router $router) {
     'uses' => 'SubscriptionsApiController@create',
   ]);
 
-  $router->put('/{criteria}', [
-    'as' => 'api.ipayu.subscriptions.update',
-    'uses' => 'SubscriptionsApiController@update',
-  ]);
-
-  $router->get('/{criteria}', [
-    'as' => 'api.ipayu.subscriptions.show',
-    'uses' => 'SubscriptionsApiController@show',
-  ]);
-
   $router->delete('/{criteria}', [
     'as' => 'api.ipayu.subscriptions.delete',
     'uses' => 'SubscriptionsApiController@delete',
