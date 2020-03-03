5e5dfd21d07970a10b38e745ddf23c5ea14667f7
YeisonTapia <yftapial@ut.edu.co>
2020-03-03 16:55:15 -0500

    Add permissions.

--- Config/permissions.php
+++ Config/permissions.php
@@ -1,5 +1,46 @@
 <?php
 
 return [
-// append
+  'ipayu.queries' => [
+    'manage' => 'ipayu::queries.manage',
+    'index' => 'ipayu::queries.list',
+    'create' => 'ipayu::queries.create',
+    'edit' => 'ipayu::queries.edit',
+    'destroy' => 'ipayu::queries.destroy',
+  ],
+  'ipayu.plans' => [
+    'manage' => 'ipayu::plans.manage',
+    'index' => 'ipayu::plans.list',
+    'create' => 'ipayu::plans.create',
+    'edit' => 'ipayu::plans.edit',
+    'destroy' => 'ipayu::plans.destroy',
+  ],
+  'ipayu.customers' => [
+    'manage' => 'ipayu::customers.manage',
+    'index' => 'ipayu::customers.list',
+    'create' => 'ipayu::customers.create',
+    'edit' => 'ipayu::customers.edit',
+    'destroy' => 'ipayu::customers.destroy',
+  ],
+  'ipayu.creditCards' => [
+    'manage' => 'ipayu::creditCards.manage',
+    'index' => 'ipayu::creditCards.list',
+    'create' => 'ipayu::creditCards.create',
+    'edit' => 'ipayu::creditCards.edit',
+    'destroy' => 'ipayu::creditCards.destroy',
+  ],
+  'ipayu.subscriptions' => [
+    'manage' => 'ipayu::subscriptions.manage',
+    'index' => 'ipayu::subscriptions.list',
+    'create' => 'ipayu::subscriptions.create',
+    'edit' => 'ipayu::subscriptions.edit',
+    'destroy' => 'ipayu::subscriptions.destroy',
+  ],
+  'ipayu.additionalCharges' => [
+    'manage' => 'ipayu::additionalCharges.manage',
+    'index' => 'ipayu::additionalCharges.list',
+    'create' => 'ipayu::additionalCharges.create',
+    'edit' => 'ipayu::additionalCharges.edit',
+    'destroy' => 'ipayu::additionalCharges.destroy',
+  ],
 ];
