3aaed46009179d5dcda72a5d5e2a25621e96559b
YeisonTapia <yftapial@ut.edu.co>
2020-03-05 18:22:53 -0500

    Add order repositories.

---
+++ Repositories/Cache/CacheOrderDecorator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Modules\Ipayu\Repositories\Cache;
+
+use Modules\Ipayu\Repositories\OrderRepository;
+use Modules\Core\Repositories\Cache\BaseCacheDecorator;
+
+class CacheOrderDecorator extends BaseCacheDecorator implements OrderRepository
+{
+    public function __construct(OrderRepository $order)
+    {
+        parent::__construct();
+        $this->entityName = 'ipayu.orders';
+        $this->repository = $order;
+    }
+
+  /**
+   * List or resources
+   *
+   * @return collection
+   */
+  public function getItemsBy($params)
+  {
+    return $this->remember(function () use ($params) {
+      return $this->repository->getItemsBy($params);
+    });
+  }
+
+  /**
+   * find a resource by id or slug
+   *
+   * @return object
+   */
+  public function getItem($criteria, $params)
+  {
+    return $this->remember(function () use ($criteria, $params) {
+      return $this->repository->getItem($criteria, $params);
+    });
+  }
+
+
+  /**
+   * create a resource
+   *
+   * @return mixed
+   */
+  public function create($data)
+  {
+    $this->clearCache();
+    return $this->repository->create($data);
+  }
+
+
+  /**
+   * update a resource
+   *
+   * @return mixed
+   */
+  public function updateBy($criteria, $data, $params)
+  {
+    $this->clearCache();
+    return $this->repository->updateBy($criteria, $data, $params);
+  }
+
+  /**
+   * destroy a resource
+   *
+   * @return mixed
+   */
+  public function deleteBy($criteria, $params)
+  {
+    $this->clearCache();
+    return $this->repository->deleteBy($criteria, $params);
+  }
+
+}
